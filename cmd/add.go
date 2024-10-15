d23943088d6aaf6b33e1006ed441f0cee03b338d
Patrick McCarty <psmccarty@wisc.edu>
2024-10-14 22:26:06 -0500

    Implement simple add and list commands

--- cmd/add.go
+++ cmd/add.go
@@ -4,6 +4,13 @@ Copyright © 2024 Patrick McCarty <patricksantos1234567@gmail.com>
 package cmd
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/psmccarty/tasks/sql/gen"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +21,23 @@ var addCmd = &cobra.Command{
 	Long:  `Adds a task to the todo list application with a short description.`,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		db, err := sql.Open("sqlite3", "tasks.db")
+		if err != nil {
+			os.Exit(1)
+		}
+		defer db.Close()
 
+		ctx := context.Background()
+		queries := gen.New(db)
+		task, err := queries.CreateTask(ctx, gen.CreateTaskParams{
+			Description:     args[0],
+			CreateTimestamp: time.Now(),
+		})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(task)
 	},
 }
 
--- cmd/list.go
+++ cmd/list.go
@@ -4,6 +4,14 @@ Copyright © 2024 Patrick McCarty <patricksantos1234567@gmail.com>
 package cmd
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"text/tabwriter"
+	"time"
+
+	"github.com/psmccarty/tasks/sql/gen"
 	"github.com/spf13/cobra"
 )
 
@@ -17,7 +25,37 @@ var listCmd = &cobra.Command{
 	Short: "Lists all current tasks",
 	Long:  `Lists all current tasks in the todo list.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		db, err := sql.Open("sqlite3", "tasks.db")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer db.Close()
 
+		ctx := context.Background()
+		queries := gen.New(db)
+
+		var tasks []gen.Task
+		if all {
+			tasks, err = queries.ListAllTasks(ctx)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		} else {
+			tasks, err = queries.ListUncompletedTasks(ctx)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
+		fmt.Fprintln(w, "ID\tTask\tCreated")
+		for _, t := range tasks {
+			fmt.Fprintf(w, "%d\t%s\t%s\n", t.ID, t.Description, t.CreateTimestamp.Format(time.RFC3339))
+		}
+		w.Flush()
 	},
 }
 

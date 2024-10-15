dadf6204b0c7bb1cf2f715d64803abc616f4383f
Patrick McCarty <psmccarty@wisc.edu>
2024-10-14 20:59:29 -0500

    create table in main

--- main.go
+++ main.go
@@ -3,8 +3,31 @@ Copyright © 2024 Patrick McCarty <patricksantos1234567@gmail.com>
 */
 package main
 
-import "github.com/psmccarty/tasks/cmd"
+import (
+	"context"
+	"database/sql"
+	_ "embed"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/psmccarty/tasks/cmd"
+)
+
+//go:embed sql/schema.sql
+var createTableIfNotExits string
 
 func main() {
+	db, err := sql.Open("sqlite3", "tasks.db")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	// create tables
+	if _, err := db.ExecContext(ctx, createTableIfNotExits); err != nil {
+		os.Exit(1)
+	}
+	db.Close()
+
 	cmd.Execute()
 }
